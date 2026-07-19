389607cc5f735263f144b6906333d9ec147a79d9
atillakhalilli0 <atilla.xalilli@gmail.com>
2026-07-19 12:43:34 +0400

    refactor: update product controller to use products service and methods

--- src/controllers/internal/products.controller.ts
+++ src/controllers/internal/products.controller.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import * as service from '../../services/orders.service.js';
+import * as service from '../../services/products.service.js';
+import { AppError } from '../../utils/appError.js';
 import { IdRequest } from '../../utils/http.js';
 
 export async function list(req: Request, res: Response, next: NextFunction) {
   try {
-    res.json(await service.listOrders(req.query));
+    res.json(await service.listProducts(req.query));
   } catch (err) {
     next(err);
   }
@@ -12,7 +13,17 @@ export async function list(req: Request, res: Response, next: NextFunction) {
 
 export async function getOne(req: IdRequest, res: Response, next: NextFunction) {
   try {
-    res.json(await service.getOrderById(req.params.id));
+    res.json(await service.getProductById(req.params.id));
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function lookupByBarcode(req: Request, res: Response, next: NextFunction) {
+  try {
+    const barcode = req.query.barcode as string;
+    if (!barcode) throw AppError.badRequest('barcode query param is required');
+    res.json(await service.getProductByBarcode(barcode, req.query.company_id as string | undefined));
   } catch (err) {
     next(err);
   }
@@ -20,9 +31,9 @@ export async function getOne(req: IdRequest, res: Response, next: NextFunction) 
 
 export async function create(req: Request, res: Response, next: NextFunction) {
   try {
-    const order = await service.createOrder(req.body);
-    req.auditContext = { companyId: order.company_id, module: 'orders', action: 'create', newValue: order };
-    res.status(201).json(order);
+    const product = await service.createProduct(req.body);
+    req.auditContext = { companyId: product.company_id, module: 'products', action: 'create', newValue: product };
+    res.status(201).json(product);
   } catch (err) {
     next(err);
   }
@@ -30,9 +41,16 @@ export async function create(req: Request, res: Response, next: NextFunction) {
 
 export async function update(req: IdRequest, res: Response, next: NextFunction) {
   try {
-    const order = await service.updateOrder(req.params.id, req.body);
-    req.auditContext = { companyId: order.company_id, module: 'orders', action: 'update', newValue: order };
-    res.json(order);
+    const before = await service.getProductById(req.params.id);
+    const product = await service.updateProduct(req.params.id, req.body);
+    req.auditContext = {
+      companyId: product.company_id,
+      module: 'products',
+      action: 'update',
+      oldValue: before,
+      newValue: product,
+    };
+    res.json(product);
   } catch (err) {
     next(err);
   }
@@ -40,24 +58,9 @@ export async function update(req: IdRequest, res: Response, next: NextFunction) 
 
 export async function remove(req: IdRequest, res: Response, next: NextFunction) {
   try {
-    await service.deleteOrder(req.params.id);
-    req.auditContext = { module: 'orders', action: 'delete', detail: `Deleted order ${req.params.id}` };
+    await service.deleteProduct(req.params.id);
+    req.auditContext = { module: 'products', action: 'delete', detail: `Deleted product ${req.params.id}` };
     res.status(204).send();
-  } catch (err) {
-    next(err);
-  }
-}
-
-export async function updateStatus(req: IdRequest, res: Response, next: NextFunction) {
-  try {
-    const order = await service.updateOrderStatus(req.params.id, req.body.status);
-    req.auditContext = {
-      companyId: order.company_id,
-      module: 'orders',
-      action: 'status_change',
-      newValue: { status: order.status },
-    };
-    res.json(order);
   } catch (err) {
     next(err);
   }
